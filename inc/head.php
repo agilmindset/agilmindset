59e7649b206a6fa46daf0158f8e3f7607113ffc6
Marcelo-Diament <marcelodiament@gmail.com>
2019-12-20 22:56:30 -0300

    update: header v1 [MB-002]

---
+++ inc/head.php
@@ -0,0 +1,17 @@
+<?php $titulo = "magicBox | Bem Vindx!"; ?>
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes, maximum-scale=5.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title><?= $titulo ?></title>
+    <meta name="title" content="<?= $titulo ?>">
+    <meta name="description" content="<?= $titulo ?> Esse site é dedicado ao compartilhamento de sugestões, ideias e insights para fins colaborativos, visando fomentar a inovação dentro do ambiente de trabalho e/ou de um produto/projeto.">
+    <link rel="pre-load" href="assets/css/style.css" />
+    <script src="https://kit.fontawesome.com/bffac1f3d7.js" crossorigin="anonymous"></script>
+    <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.css" integrity="sha256-46qynGAkLSFpVbEBog43gvNhfrOj+BmwXdxFgVK/Kvc=" crossorigin="anonymous" /> -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
