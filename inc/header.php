0f224c54414ffdb774f98bcd9886e9ed9a5486e4
Marcelo-Diament <marcelodiament@gmail.com>
2019-12-20 23:13:28 -0300

    update: header v1 [MB-004]

---
+++ inc/header.php
@@ -0,0 +1,22 @@
+<body>
+    <header id="headerTopo">
+        <nav class="navbar navbar-expand-lg fixed-top">
+            <a class="navbar-brand" href="./" title="Acesse a Home da magicBox">
+                <img src="https://compracerta.vteximg.com.br/arquivos/compracerta2-favicon-32x32.png" alt="Logo Compra Certa">
+                magicBox <small>| Ideias&Insights</small>
+            </a>
+            <button class="navbar-toggler custom-toggler" type="button" data-toggle="collapse" data-target="#menuTopo" aria-controls="menuTopo" aria-expanded="false" aria-label="Toggle navigation" title="Acessar submenu">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="menuTopo">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item ml-auto">
+                        <a class="nav-link" href="./cadastrar.php" title="Cadastre-se">Cadastre-se</a>
+                    </li>
+                    <li class="nav-item ml-auto">
+                        <a class="nav-link" href="./login.php" title="Faça Login">Login</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
