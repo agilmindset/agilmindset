bd8b9fc2b0188546f4d3b2234d98af58d0f7fbdf
Marcelo-Diament <marcelodiament@gmail.com>
2019-12-23 20:21:03 -0300

    update: htaccess and some few adjusts [MB-005]

--- inc/head.php
+++ inc/head.php
@@ -8,9 +8,9 @@
     <meta name="title" content="<?= $tituloPagina ?>">
     <meta name="description" content="<?= $tituloPagina . "" . $descricaoPagina ?>">
     <link rel="pre-load" href="assets/css/style.css" />
-    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.12.0/css/all.css" integrity="sha384-ekOryaXPbeCpWQNxMwSWVvQ0+1VrStoPJq54shlYhR8HzQgig1v5fas6YgOqLoKz" crossorigin="anonymous">
+    <!-- <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.12.0/css/all.css" integrity="sha384-ekOryaXPbeCpWQNxMwSWVvQ0+1VrStoPJq54shlYhR8HzQgig1v5fas6YgOqLoKz" crossorigin="anonymous"> -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <link async href="https://fonts.googleapis.com/css?family=Bebas+Neue|Comfortaa:500&display=swap" rel="stylesheet">
+    <link async href="https://fonts.googleapis.com/css?family=Comfortaa:500&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="assets/css/style.css">
     <link rel="icon" href="assets/img/favicon.svg" sizes="48x48"/>
 </head>
