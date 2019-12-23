e9495d0c3bde297a6ad2ec7a4b0a543965fbcf38
Marcelo-Diament <marcelodiament@gmail.com>
2019-12-23 18:50:24 -0300

    update: banner topo redesign third round [MB-005]

--- inc/banner-topo.php
+++ inc/banner-topo.php
@@ -5,9 +5,9 @@
                 <img srcset="assets/img/agil-mindset.jpg 480w" sizes="480px" src="assets/img/agil-mindset.jpg" class="col-12 col-sm-5 col-md-3 px-0 pl-md-0 pr-md-3 banner-img" alt="agilMindset | Uma plataforma para dividirmos insights e sugerirmos projetos inovadores">
                 <div class="banner-caption col-12 col-sm-7 col-md-9 p-4">
                     <h1 class="">agil<wbr/><span class="box">mindset</span></h1>
-                    <p class="">Ferramentas digitais para ajudar empresas a se destacarem</p>
-                    <!-- <a href="./login.php" class="btn btn-dark" title="Faça Login" rel="next">Login</a>
-                    <a href="./cadastro.php" class="btn btn-dark" title="Cadastre-se" rel="next">Cadastro</a> -->
+                    <p class="">Processos que geram <strong>valor</strong> e <strong>inovação</strong> através do <strong>engajamento</strong> e da <strong>colaboração</strong></p>
+                    <a href="./demo.php" target="_blank" class="btn btn-dark" title="Visite a versão demo" rel="noopener">Acessar versão Demo</a>
+                    <!-- <a href="./cadastro.php" class="btn btn-dark" title="Cadastre-se e cadastre seu time!" rel="next">Cadastre-se</a> -->
                 </div>
             </article>
         </section>
@@ -15,8 +15,17 @@
             <article class="d-flex flex-wrap flex-md-nowrap align-items-center p-0">
                 <img srcset="assets/img/ponte.jpg 480w" sizes="480px" src="assets/img/ponte.jpg" class="col-12 col-sm-5 col-md-3 px-0 pl-md-0 pr-md-3 banner-img" alt="Pesquise ideias | Busque por ideia através de filtros e categorias">
                 <div class="banner-caption col-12 col-sm-7 col-md-9 p-4">
-                    <h1 class="">Aproximamos</h1>
-                    <p class="">Conhecimento de clientes e colaboradores à sua marca</p>
+                    <h1 class="">Aproximamos<br/><span class="box"><strong>usuários e colaboradores</strong></span></h1>
+                    <p class="">Capturamos <strong>dados relevantes</strong> sobre o <strong>público</strong> através de <strong>pesquisas gamificadas</strong></p>
+                </div>
+            </article>
+        </section>
+        <section class="carousel-item">
+            <article class="d-flex flex-wrap flex-md-nowrap align-items-center p-0">
+                <img srcset="assets/img/ideias-colaborativas.jpg 480w" sizes="480px" src="assets/img/ideias-colaborativas.jpg" class="col-12 col-sm-5 col-md-3 px-0 pl-md-0 pr-md-3 banner-img" alt="Colabore também | Envie novas ideias ou dê sugestões à ideias já cadastradas">
+                <div class="banner-caption col-12 col-sm-7 col-md-9 p-4">
+                    <h1 class="">Incentivamos o<br/><span class="box"><strong>compartilhamento de ideias</strong></span></h1>
+                    <p class="">Clientes e colaboradores de diferentes departamentos podem <strong>dividir ideias</strong>, sugerir <strong>melhorias</strong> e <strong>propor mudanças</strong></p>
                 </div>
             </article>
         </section>
@@ -24,8 +33,26 @@
             <article class="d-flex flex-wrap flex-md-nowrap align-items-center p-0">
                 <img srcset="assets/img/criacao-de-personas.jpg 480w" sizes="480px" src="assets/img/criacao-de-personas.jpg" class="col-12 col-sm-5 col-md-3 px-0 pl-md-0 pr-md-3 banner-img" alt="Colabore também | Envie novas ideias ou dê sugestões à ideias já cadastradas">
                 <div class="banner-caption col-12 col-sm-7 col-md-9 p-4">
-                    <h1 class="">Conheça seu público de verdade</h1>
-                    <p class="">Conte com reports dinâmicos e ferramentas de Design Thinking</p>
+                    <h1 class="">Identificamos<br/><span class="box"><strong>perfis de clientes</strong></span></h1>
+                    <p class="">Conte com painéis e métodos de <strong>Design Thinking</strong> para entender os <strong>desejos e preferências</strong> de cada perfil</p>
+                </div>
+            </article>
+        </section>
+        <section class="carousel-item">
+            <article class="d-flex flex-wrap flex-md-nowrap align-items-center p-0">
+                <img srcset="assets/img/dados-em-conhecimento.jpg 480w" sizes="480px" src="assets/img/dados-em-conhecimento.jpg" class="col-12 col-sm-5 col-md-3 px-0 pl-md-0 pr-md-3 banner-img" alt="Colabore também | Envie novas ideias ou dê sugestões à ideias já cadastradas">
+                <div class="banner-caption col-12 col-sm-7 col-md-9 p-4">
+                    <h1 class="">Transformamos<br/><span class="box"><strong>dados em conhecimento</strong></span></h1>
+                    <p class="">Através de <strong>Data Analytics</strong>, tangibilizamos dores e anseios de <strong>clientes e colaboradores</strong> - <strong>oportunidades</strong></p>
+                </div>
+            </article>
+        </section>
+        <section class="carousel-item">
+            <article class="d-flex flex-wrap flex-md-nowrap align-items-center p-0">
+                <img srcset="assets/img/projetos-ageis.jpg 480w" sizes="480px" src="assets/img/projetos-ageis.jpg" class="col-12 col-sm-5 col-md-3 px-0 pl-md-0 pr-md-3 banner-img" alt="Colabore também | Envie novas ideias ou dê sugestões à ideias já cadastradas">
+                <div class="banner-caption col-12 col-sm-7 col-md-9 p-4">
+                    <h1 class="">Fomentamos<br/><span class="box"><strong>projetos colaborativos</strong></span></h1>
+                    <p class="">Uma plataforma de <strong>cocriação</strong> para implantação de <strong>protótipos</strong> e <strong>MVP</strong> com <strong>métodos ágeis</strong></p>
                 </div>
             </article>
         </section>
