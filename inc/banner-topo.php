5cf8ff4eb8cc9eb0e8071a405e34be21f3135454
Marcelo-Diament <marcelodiament@gmail.com>
2019-12-23 17:12:51 -0300

    update: banner topo redesign second round [MB-005]

--- inc/banner-topo.php
+++ inc/banner-topo.php
@@ -1,28 +1,34 @@
-<section id="carouselPrincipal" class="row carousel slide" data-ride="carousel">
+<section id="carouselPrincipal" class="row carousel slide carousel-fade" data-ride="carousel">
     <article class="carousel-inner">
-        <div class="carousel-item active d-flex flex-wrap flex-md-nowrap align-items-center p-0">
-            <img srcset="assets/img/agil-mindset.jpg 480w" sizes="480px" src="assets/img/agil-mindset.jpg" class="col-12 col-sm-5 col-md-3 px-0 pl-md-0 pr-md-3 banner-img" alt="agilMindset | Uma plataforma para dividirmos insights e sugerirmos projetos inovadores">
-            <div class="banner-caption col-12 col-sm-7 col-md-9 p-4">
-                <h1 class="">agil<wbr/><span class="box">mindset</span></h1>
-                <p class="">Ferramentas digitais para ajudar empresas a se destacarem</p>
-                <!-- <a href="./login.php" class="btn btn-dark" title="Faça Login" rel="next">Login</a>
-                <a href="./cadastro.php" class="btn btn-dark" title="Cadastre-se" rel="next">Cadastro</a> -->
-            </div>
-        </div>
-        <!-- <div class="carousel-item">
-        <img srcset="assets/img/ponte-xl.jpg 1140w, assets/img/ponte-lg.jpg 960w, assets/img/ponte-md.jpg 720w, assets/img/ponte-sm.jpg 540w, assets/img/ponte-xs.jpg 480w" sizes="(min-width: 1200px) 1140px, (min-width: 992px) 960px, (min-width: 768px) 720px, (min-width: 576px) 540px, 480px" src="assets/img/ponte-lg.jpg" class="d-block w-100" alt="Pesquise ideias | Busque por ideia através de filtros e categorias">
-        <div class="carousel-caption d-none d-sm-block">
-            <h1 class="text-dark">Aproximamos</h1>
-            <p class="text-dark">Conhecimento de clientes e colaboradores à sua marca</p>
-        </div>
-        </div>
-        <div class="carousel-item">
-        <img srcset="assets/img/criacao-de-personas-xl.jpg 1140w, assets/img/criacao-de-personas-lg.jpg 960w, assets/img/criacao-de-personas-md.jpg 720w, assets/img/criacao-de-personas-sm.jpg 540w, assets/img/criacao-de-personas-xs.jpg 480w" sizes="(min-width: 1200px) 1140px, (min-width: 992px) 960px, (min-width: 768px) 720px, (min-width: 576px) 540px, 480px" src="assets/img/criacao-de-personas-lg.jpg" class="d-block w-100" alt="Colabore também | Envie novas ideias ou dê sugestões à ideias já cadastradas">
-        <div class="carousel-caption d-none d-sm-block">
-            <h1 class="text-dark">Conheça seu público de verdade</h1>
-            <p class="text-dark">Conte com reports dinâmicos e ferramentas de Design Thinking</p>
-        </div>
-        </div> -->
+        <section class="carousel-item active">
+            <article class="d-flex flex-wrap flex-md-nowrap align-items-center p-0">
+                <img srcset="assets/img/agil-mindset.jpg 480w" sizes="480px" src="assets/img/agil-mindset.jpg" class="col-12 col-sm-5 col-md-3 px-0 pl-md-0 pr-md-3 banner-img" alt="agilMindset | Uma plataforma para dividirmos insights e sugerirmos projetos inovadores">
+                <div class="banner-caption col-12 col-sm-7 col-md-9 p-4">
+                    <h1 class="">agil<wbr/><span class="box">mindset</span></h1>
+                    <p class="">Ferramentas digitais para ajudar empresas a se destacarem</p>
+                    <!-- <a href="./login.php" class="btn btn-dark" title="Faça Login" rel="next">Login</a>
+                    <a href="./cadastro.php" class="btn btn-dark" title="Cadastre-se" rel="next">Cadastro</a> -->
+                </div>
+            </article>
+        </section>
+        <section class="carousel-item">
+            <article class="d-flex flex-wrap flex-md-nowrap align-items-center p-0">
+                <img srcset="assets/img/ponte.jpg 480w" sizes="480px" src="assets/img/ponte.jpg" class="col-12 col-sm-5 col-md-3 px-0 pl-md-0 pr-md-3 banner-img" alt="Pesquise ideias | Busque por ideia através de filtros e categorias">
+                <div class="banner-caption col-12 col-sm-7 col-md-9 p-4">
+                    <h1 class="">Aproximamos</h1>
+                    <p class="">Conhecimento de clientes e colaboradores à sua marca</p>
+                </div>
+            </article>
+        </section>
+        <section class="carousel-item">
+            <article class="d-flex flex-wrap flex-md-nowrap align-items-center p-0">
+                <img srcset="assets/img/criacao-de-personas.jpg 480w" sizes="480px" src="assets/img/criacao-de-personas.jpg" class="col-12 col-sm-5 col-md-3 px-0 pl-md-0 pr-md-3 banner-img" alt="Colabore também | Envie novas ideias ou dê sugestões à ideias já cadastradas">
+                <div class="banner-caption col-12 col-sm-7 col-md-9 p-4">
+                    <h1 class="">Conheça seu público de verdade</h1>
+                    <p class="">Conte com reports dinâmicos e ferramentas de Design Thinking</p>
+                </div>
+            </article>
+        </section>
     </article>
     <a class="carousel-control-prev" href="#carouselPrincipal" role="button" data-slide="prev" title="Ver slide anterior" rel="prev">
         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
