739987a4d831a5c6504e0de27f5401f18cdd8dae
Marcelo-Diament <marcelodiament@gmail.com>
2019-12-24 00:08:40 -0300

    update: favicon updated - images and head links [MB-006]

--- inc/head.php
+++ inc/head.php
@@ -12,7 +12,24 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link async href="https://fonts.googleapis.com/css?family=Comfortaa:500&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="assets/css/style.css">
-    <link rel="icon" href="assets/img/favicon-48x48.png" sizes="48x48"/>
-    <link rel="icon" href="assets/img/favicon-72x72.png" sizes="72x72"/>
-    <link rel="icon" href="assets/img/favicon-512x512.png" sizes="512x512"/>
+    <meta name="msapplication-TileImage" content="favicons/tile.png">
+    <!-- Favicons -->
+    <link rel="icon" type="image/png" sizes="512x512" href="favicons/favicon-512x512.png">
+    <link rel="icon" type="image/png" sizes="228x228" href="favicons/favicon-228x228.png">
+    <link rel="shortcut icon" sizes="196x196" href="favicons/favicon-196.png">
+    <link rel="icon" sizes="192x192" href="favicons/touch-icon-192x192.png">
+    <link rel="apple-touch-icon-precomposed" sizes="180x180" href="favicons/apple-touch-icon-precomposed-180x180.png">
+    <link rel="apple-touch-icon-precomposed" sizes="152x152" href="favicons/apple-touch-icon-precomposed-152x152.png">
+    <link rel="icon" type="image/png" sizes="128x128" href="favicons/favicon-128x128.png">
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="favicons/apple-touch-icon-precomposed-144x144.png">
+    <link rel="apple-touch-icon-precomposed" sizes="120x120" href="favicons/apple-touch-icon-precomposed-120x120.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="favicons/apple-touch-icon-precomposed-114x114.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="favicons/favicon-96x96.png">
+    <link rel="apple-touch-icon-precomposed" sizes="76x76" href="favicons/apple-touch-icon-precomposed-76x76.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="favicons/apple-touch-icon-precomposed-72x72.png">
+    <link rel="apple-touch-icon-precomposed" sizes="60x60" href="favicons/apple-touch-icon-precomposed-60x60.png">
+    <link rel="apple-touch-icon-precomposed" sizes="57x57" href="favicons/apple-touch-icon-precomposed-57x57.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="favicons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="favicons/favicon-16x16.png">
+    <!-- /Favicons -->
 </head>
