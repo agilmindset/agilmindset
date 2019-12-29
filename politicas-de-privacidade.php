3523754d9a213751a04906f9d4346540033a89fc
Marcelo-Diament <marcelodiament@gmail.com>
2019-12-29 17:07:45 -0300

    update: layout fixes and compressed images uploda [MB-012]

--- politicas-de-privacidade.php
+++ politicas-de-privacidade.php
@@ -9,9 +9,13 @@ $urlAmigavelPagina = "https://agilmindset.com/politicas-de-privacidade";
 <body>
     <?php require_once("./inc/header.php"); ?>
     <main class="container-fluid px-0 politicas">
-        <section class="container-fluid mx-0 p-5">
+        <section class="container-fluid mx-0 p-2 p-sm-4 p-md-5">
             <article class="d-flex flex-wrap flex-md-nowrap align-items-center p-0">
-                <img srcset="assets/img/erro-404.jpg 480w" sizes="480px" src="./assets/img/erro-404.jpg" class="col-12 col-sm-5 col-md-3 px-0 pl-md-0 pr-md-3 banner-img" alt="agilMindset | Erro 404 - Estamos trabalhando para resolvermos a questão.">
+                
+
+
+                <img srcset="./assets/img/politicas-de-privacidade-150x150.jpg 150w, ./assets/img/politicas-de-privacidade-200x200.jpg 200w, ./assets/img/politicas-de-privacidade-250x250.jpg 250w, ./assets/img/politicas-de-privacidade-300x300.jpg 300w, ./assets/img/politicas-de-privacidade-350x350.jpg 350w, ./assets/img/politicas-de-privacidade-400x400.jpg 400w, ./assets/img/politicas-de-privacidade-450x450.jpg 450w, ./assets/img/politicas-de-privacidade-500x500.jpg 500w, ./assets/img/politicas-de-privacidade-550x550.jpg 550w, ./assets/img/politicas-de-privacidade-574x574.jpg 574w, ./assets/img/politicas-de-privacidade-267x267.jpg 267w, ./assets/img/politicas-de-privacidade-300x300.jpg 300w, ./assets/img/politicas-de-privacidade-224x224.jpg 224w, ./assets/img/politicas-de-privacidade-254x254.jpg 254w, ./assets/img/politicas-de-privacidade-304x304.jpg 304w, ./assets/img/politicas-de-privacidade-344x344.jpg 344w, ./assets/img/politicas-de-privacidade-464x464.jpg 464w, ./assets/img/politicas-de-privacidade-484x484.jpg 484w, ./assets/img/politicas-de-privacidade-614x614.jpg 614w" sizes="(min-width: 2500px) 614px, (min-width: 2000px) 484px, (min-width: 1920px) 464px, (min-width: 1440px) 344px, (min-width: 1280px) 304px, (min-width: 1080px) 254px, (min-width: 960px) 224px, (min-width: 720px) 300px, (min-width: 640px) 267px, (min-width: 574px) 574px, (min-width: 550px) 550px, (min-width: 500px) 500px, (min-width: 450px) 450px, (min-width: 400px) 400px, (min-width: 350px) 350px, (min-width: 300px) 300px, (min-width: 250px) 250px, (min-width: 200px) 200px, 150px" src="./assets/img/politicas-de-privacidade.jpg" class="col-12 col-sm-5 col-md-3 px-0 pl-md-0 pr-md-3 banner-img" alt="Políticas de Privacidade - entenda como os dados capturados na plataforma são utilizados pela agilmindset">
+                
                 <div class="col-12 col-sm-7 col-md-9 p-4">
                     <h1 class="">Políticas<br/><span class="box">de Privacidade</span></h1>
                     <p class="mt-5">A <b>agilmindset</b> zela pela <b>transparência</b> junto a seus colaboradores, clientes, usuários e parceiros. E é por isso que nossa página de <b>políticas de privacidade</b> foi elaborada de forma a ser <b>facil e claramente compreendida</b>, sem <i>juridiquês</i> nem delongas. =)</p>
@@ -20,12 +24,12 @@ $urlAmigavelPagina = "https://agilmindset.com/politicas-de-privacidade";
                 </div>
             </article>
         </section>
-        <section class="container-fluid mx-0 p-5 bg-light">
+        <section class="container-fluid mx-0 p-2 p-sm-4 p-md-5 bg-light">
             <article class="container">
                 <section class="row my-3">
                     <header class="col-12 p-5">
                         <h3 class="mb-3"><b>TL; DR</b></h3>
-                        <ul style="list-style-type: disc" class="pl-4 mb-0">
+                        <ul style="list-style-type: disc" class="pl-1 pl-sm-3 pl-md-4 mb-0">
                             <li class="my-2">Todas as informações disponibilizadas na plataforma serão registradas em um banco de dados para que esses dados sejam transformados em oportunidades;</li>
                             <li class="my-2">Os dados obtidos pela plataforma (bem como relatórios) serão sempre compartilhados com a <b>agilmindset</b> (que não distribuirá a terceiros em hipótese alguma);</li>
                             <li class="my-2">Dados pessoais serão preservados, sendo as senhas criptografadas (aconselhamos o uso de senhas fortes);</li>
