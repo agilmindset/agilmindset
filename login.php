ad1d7d88ec7cae5d418734c97c73d973330c7cea
Marcelo-Diament <marcelodiament@gmail.com>
2019-12-28 23:54:27 -0300

    update: login page initial version based on register page [MB-011]

---
+++ login.php
@@ -0,0 +1,21 @@
+<?php
+$tituloPagina = "agil mindset | Login de Usuário";
+$descricaoPagina = "Faça Login e acesse todas as ferramentas da agil mindset em uma versão de demonstração";
+$urlPagina = "https://agilmindset.com/login.php";
+$urlAmigavelPagina = "https://agilmindset.com/login";
+?>
+<?php require_once("./inc/head.php"); ?>
+
+<body>
+    <?php require_once("./inc/header.php"); ?>
+    <main class="container-fluid px-0">
+        <?php require_once("./inc/form-login.php"); ?>
+        <?php require_once("./inc/banner-secundario.php"); ?>
+    </main>
+    <?php require_once("./inc/footer.php"); ?>
+</body>
+
+</html>
+
+
+
