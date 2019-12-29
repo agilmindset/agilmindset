d3e9bff537e9ee5ffa34adc46f07cad1262be58d
Marcelo-Diament <marcelodiament@gmail.com>
2019-12-29 17:19:14 -0300

    update: page file and basic structured copied from politicas-de-privacidade [MB-013]

---
+++ termos-de-uso.php
@@ -0,0 +1,93 @@
+<?php
+$tituloPagina = "agil mindset | Termos de Uso";
+$descricaoPagina = "Entenda os termos de uso na plataforma agilmindset.";
+$urlPagina = "https://agilmindset.com/termos-de-uso.php";
+$urlAmigavelPagina = "https://agilmindset.com/termos-de-uso";
+?>
+<?php require_once("./inc/head.php"); ?>
+
+<body>
+    <?php require_once("./inc/header.php"); ?>
+    <main class="container-fluid px-0 politicas">
+        <section class="container-fluid mx-0 p-2 p-sm-4 p-md-5">
+            <article class="d-flex flex-wrap flex-md-nowrap align-items-center p-0">
+                
+
+
+                <img srcset="./assets/img/termos-de-uso-150x150.jpg 150w, ./assets/img/termos-de-uso-200x200.jpg 200w, ./assets/img/termos-de-uso-250x250.jpg 250w, ./assets/img/termos-de-uso-300x300.jpg 300w, ./assets/img/termos-de-uso-350x350.jpg 350w, ./assets/img/termos-de-uso-400x400.jpg 400w, ./assets/img/termos-de-uso-450x450.jpg 450w, ./assets/img/termos-de-uso-500x500.jpg 500w, ./assets/img/termos-de-uso-550x550.jpg 550w, ./assets/img/termos-de-uso-574x574.jpg 574w, ./assets/img/termos-de-uso-267x267.jpg 267w, ./assets/img/termos-de-uso-300x300.jpg 300w, ./assets/img/termos-de-uso-224x224.jpg 224w, ./assets/img/termos-de-uso-254x254.jpg 254w, ./assets/img/termos-de-uso-304x304.jpg 304w, ./assets/img/termos-de-uso-344x344.jpg 344w, ./assets/img/termos-de-uso-464x464.jpg 464w, ./assets/img/termos-de-uso-484x484.jpg 484w, ./assets/img/termos-de-uso-614x614.jpg 614w" sizes="(min-width: 2500px) 614px, (min-width: 2000px) 484px, (min-width: 1920px) 464px, (min-width: 1440px) 344px, (min-width: 1280px) 304px, (min-width: 1080px) 254px, (min-width: 960px) 224px, (min-width: 720px) 300px, (min-width: 640px) 267px, (min-width: 574px) 574px, (min-width: 550px) 550px, (min-width: 500px) 500px, (min-width: 450px) 450px, (min-width: 400px) 400px, (min-width: 350px) 350px, (min-width: 300px) 300px, (min-width: 250px) 250px, (min-width: 200px) 200px, 150px" src="./assets/img/termos-de-uso.jpg" class="col-12 col-sm-5 col-md-3 px-0 pl-md-0 pr-md-3 banner-img" alt="Termos de Uso - entenda quais são os termos e condições para o uso da plataforma agilmindset">
+                
+                <div class="col-12 col-sm-7 col-md-9 p-4">
+                    <h1 class="">Termos<br/><span class="box">e Condições de Uso</span></h1>
+                    <p class="mt-5">A <b>agilmindset</b> proporciona um ambiente digital de <b>compartilhamento, cocriação, de coexistência</b>. E para que surjam projetos inovadores e que agreguem valor aos negócios, é imprescindível a boa convivência entre todos os usuários. Para garantir essa <b>boa convivência</b>, elaboramos esses <b>Termos de Uso</b> (ou Termos e Condiçõe de Uso).</p>
+                    <p>Se houver qualquer dúvida, conte conosco! Basta <a href="mailto:webmaster@agilmindset.com?subject=Mensagem%20enviada%20via%20site%20agilMindset" rel="noopener noreferrer" target="_blank" title="Enviar email para webmaster@agilmindset.com">entrar em contato</a> - teremos prazer em ajudar você!</p>
+                    <p class="mb-5"><b>Lembre-se de que ao navegar no site, você está ciente e de acordo com nossas Políticas de Privacidade e <a href="./termos-de-uso.php" target="_blank" rel="noopener noreferrer">Termos de Uso</a>.</b></p>
+                </div>
+            </article>
+        </section>
+        <section class="container-fluid mx-0 p-2 p-sm-4 p-md-5 bg-light">
+            <article class="container">
+                <!-- <section class="row my-3">
+                    <header class="col-12 p-5">
+                        <h3 class="mb-3"><b>TL; DR</b></h3>
+                        <ul style="list-style-type: disc" class="pl-1 pl-sm-3 pl-md-4 mb-0">
+                            <li class="my-2">Todas as informações disponibilizadas na plataforma serão registradas em um banco de dados para que esses dados sejam transformados em oportunidades;</li>
+                            <li class="my-2">Os dados obtidos pela plataforma (bem como relatórios) serão sempre compartilhados com a <b>agilmindset</b> (que não distribuirá a terceiros em hipótese alguma);</li>
+                            <li class="my-2">Dados pessoais serão preservados, sendo as senhas criptografadas (aconselhamos o uso de senhas fortes);</li>
+                            <li class="my-2">O site conta com segurança certificada através do selo SSL;</li>
+                            <li class="my-2">Adquirentes da plataforma poderão definir as próprias políticas e termos em suas instâncias (políticas essas que não se sobreporão jamais às definidas pela própria plataforma);</li>
+                            <li class="my-2">Ao navegar no site, o usuário está ciente e de acordo com os termos e políticas do site.</li>
+                        </ul>
+                    </header>
+                </section> -->
+                <section class="row my-3">
+                    <h2 class="col-12 mt-4">Termos Gerais</h2>
+                    <article class="col-12 my-3">
+                        <p class="col-12 mt-3">A <b>agilmindset</b> é uma iniciativa da <b>Djament Comunicação</b> (razão social Marcelo Diament37484380873, inscrita como MEI sob o CNPJ 20.196.856/0001-91). Trata-se de um projeto que <b>visa fomentar a cultura da inovação através do desenvolvimento e fornecimento de ferramentas digitais que tornem processos criativos e colaborativos mais acessíveis</b> a empresas e atores de projetos que buscam a inovação.</p>
+                        <p class="col-12">Parte dessas ferramentas digitais é focada na criação de ideias (da publicação de artigos à definição de protótipos e MVPs) e outra parte é direcionada à gestão dos dados obtidos através da plataforma (seja por quizes, inputs de terceiros, formulários...). <b>O intuito é: de um lado obter dados relevantes da forma mais segmentada possível e, do outro, transformar esses dados em conhecimento</b> (em forma de estatísticas, de personas, do detalhamento da jornada de experiência do consumidor, insights de colaboradores...).</p>
+                        <p class="col-12 mb-3">O projeto tem como premissa a gamificação dos processos, de forma a torná-los mais atraentes, envolventes e até mesmo mais valorizados. Para isso, praticamente toda ação executada na plataforma (enquanto usuário que fornece informações) é recompensada através de pontos (a serem definidos por cada adquirente da plataforma). Torna-se então <b>necessário o registro das ações realizadas atreladas ao usuário que as realizou, horário e outras informações que permitirão inclusise o repasse dos pontos ao usuário</b> (a troca dos pontos por prêmios é definida pelo adquirente da plataforma).</p>
+                    </article>
+                </section>
+                <section class="row my-3">
+                    <h2 class="col-12 mt-4">Privacidade</h2>
+                    <article class="col-12 my-3">
+                        <p class="col-12 my-3">A Djament Comunicação ("<b>agilmindset</b>") tem como compromisso o respeito pelas informações de seus visitantes, usuários, clientes e parceiros. Recomendamos fortemente a leitura de nossas políticas. Ao fornecer informações pessoais e/ou navegar no site, você está ciente e de acordo com as regras de utilização, proteção e segurança aqui estabelecidas.</p>
+                    </article>
+                    <article class="col-12 my-3">
+                        <h3 class="text-center mt-1 mb-4">Coleta de Dados</h3>
+                        <p class="col-12 mt-3">Ao se cadastrar na <b>agilmindset</b>, é necessário o fornecimento de dados como nome completo, endereço de email apelido e senha definida no momento do cadastro. Essas informações são armazenadas pela <b>agilmindset</b> com o objetivo de otimizar a jornada de experiência (além da própria proteção de seus dados). Além dos dados básicos de cadastro, a <b>agilmindset</b> coleta algumas outras informações de forma automática, como o país de acesso, data e hora de acesso, o dispositivo utilizado, etc..</p>
+                        <p class="col-12">Um recurso fundamental da plataforma são os quizes - formulários gamificados que captam dados relativos à jornada de experiência do consumidor, recompensando os usuários que respondem às questões com pontos (que posteriormente serão trocados por benefícios acordados com os gestores dos adquirentes da plataforma). Esses dados serão formatados, higienizados, segmentados e expostos em forma de gráficos e relatórios - nunca atrelados diretamente à pessoa que respondeu ao quiz.</p>
+                        <p class="col-12 mb-3">Cada adquirente da plataforma terá uma instância personalizada da mesma. Para cada instância da plataforma, o adquirente poderá definir suas próprias políticas - de privacidade, de troca de pontos, de termos de uso... no entanto, em caso de conflitos, a política definida pela <b>agilmindset</b> sempre deverá prevalecer.</p>
+                    </article>
+                    <article class="col-12 my-3">
+                        <h3 class="text-center mt-1 mb-4">Uso de Dados</h3>
+                        <p class="col-12 mt-3">A <b>agilmindset</b> entende que todas as informações pessoais coletadas como confidenciais, expondo apenas dados referentes a hábitos, costumes e comportamento - sem nunca divulgar nomes, emails e documentos de usuários reais.</p>
+                        <p class="col-12 mb-3">Sendo assim, ao navegar no site e utilizar os serviços da <b>agilmindset</b>, o usuário consente expressamente com o uso de seus dados e informações da forma prevista em nossas políticas, tanto para efeitos de uso da plataforma como para otimizar e melhorar continuamente a experiência do usuário em nosso site.</p>
+                    </article>
+                    <article class="col-12 my-3">
+                        <h3 class="text-center mt-1 mb-4">Exclusão de Conta</h3>
+                        <p class="col-12 mt-3">É possível solicitar a exclusão - definitiva - dos <b>dados pessoais</b>, a <b>agilmindset</b> a fará conforme a Lei 12.965/2014, respeitando o prazo mínimo legal de armazenamento de informações. Para solicitar a exclusão é necessário <a href="mailto:webmaster@agilmindset.com?subject=agilmindset%20-%20Solicitação%20de%20exclusão%20de%20cadastro%20-%20<?php echo date('d/m/Y'); ?>" rel="noopener noreferrer" target="_blank" title="Enviar email para webmaster@agilmindset.com">entre em contato conosco</a>.</p>
+                        <p class="col-12 mb-3">Vale ressaltar que dados não pessoais coletados (respostas à pesquisas e quizes, por exemplo), serão mantidos em nossa base de dados.</p>
+                    </article>
+                    <article class="col-12 my-3">
+                        <h3 class="text-center mt-1 mb-4">Cookies</h3>
+                        <p class="col-12 my-3">O uso de cookies permite ao site reconhecer um usuário, permitindo aplicar suas preferências e melhorar sua experiência de forma geral. Trata-se de arquivos de dados que são armazenados no computador (sem dados pessoais). Alguns desses dados são definidos diretamente pela <b>agilmindset</b> (como a preferência de filtros de busca, por exemplo) enquanto outros são definidos por terceiros (como o Google Analytics, que monitora o comportamento de usuários no site para identificar possíveis erros e melhorias possíveis).</p>
+                    </article>
+                </section>
+                <section class="row my-3">
+                    <h2 class="col-12 mt-4">Atualizações</h2>
+                    <article class="col-12 my-3">
+                        <p class="col-12 my-3">A Djament Comunicação ("<b>agilmindset</b>") reserva-se o direito de, a qualquer momento, modificar as Políticas de Privacidade aqui definidas.</p>
+                    </article>
+                    <article class="col-12 my-3">
+                        <h3 class="text-center mt-1 mb-4">Legislação Aplicável</h3>
+                        <p class="col-12 my-3">Essas <b>Políticas de Privacidade</b> são regidas, interpretadas e executadas conforme as leis da República Federativa do Brasil. O usuário consente, expressa e voluntariamente, com a competência desse juízo e renuncia à competência de quaisquer outros, por mais privilegiado que possa ser ou que possa vir a ser.</p>
+                    </article>
+                </section>
+            </article>
+        </section>
+        <?php require_once("./inc/banner-secundario.php"); ?>
+    </main>
+    <?php require_once("./inc/footer.php"); ?>
+</body>
+
+</html>
