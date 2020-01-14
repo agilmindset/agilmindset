6875fea4257118bab5ffef2079c40995eefb3796
Marcelo-Diament <marcelodiament@gmail.com>
2020-01-14 18:28:03 -0300

    update: top container adjusted to different breakpoints [MB-017]

--- inc/head.php
+++ inc/head.php
@@ -2,13 +2,15 @@
 <html lang="pt-BR" prefix="og: http://ogp.me/ns#">
 
 <head>
-    <!-- Google Tag Manager -->
-    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-TQBBNJC');</script>
-    <!-- End Google Tag Manager -->
+    <?php if( strpos($_SERVER["SERVER_NAME"],"localhost") ||  strpos($_SERVER["SERVER_NAME"],"127.0.0.1") || strpos($_SERVER["REQUEST_URI"],"magicbox")) { ?>
+        <!-- Google Tag Manager -->
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-TQBBNJC');</script>
+        <!-- End Google Tag Manager -->
+    <?php } ?>
     <!-- Configurações Gerais -->
     <meta charset="UTF-8">
     <!-- <meta http-equiv="Content-Type" content="text/html; charset=utf-8"> -->
