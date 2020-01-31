4bf29c2e2d4e1d1db4aacb5e0ee3685336ce59ae
Marcelo-Diament <marcelodiament@gmail.com>
2020-01-31 13:53:13 -0300

    update: cleaning unecessary comments [MB-018]

--- utils/registrarUsuario.php
+++ utils/registrarUsuario.php
@@ -2,8 +2,6 @@
 
   require_once("../config/conn.php");
 
-    //   var_dump($_POST);
-    //   exit;
     $nome = $_POST["inputNome"];
     $sobrenome = $_POST["inputSobrenome"];
     $apelido = $_POST["inputApelido"];
@@ -15,8 +13,6 @@
         $aceite = 0;
     }
     $dominio = substr($_POST["inputEmail"],strpos($_POST["inputEmail"], "@"));
-    // var_dump($dominio);
-    // exit;
     if($_POST["inputEmail"] === "marcelo@djament.com.br" || $_POST["inputEmail"] === "webmaster@agilmindset.com") {
         $nivel = 99;
     } else if($dominio === "@djament.com.br" || $dominio === "@agilmindset.com"){
@@ -24,27 +20,11 @@
     } else {
         $nivel = 10;
     }
-    // var_dump($_POST["inputNome"]);
-    // var_dump($_POST["inputSobrenome"]);
-    // var_dump($_POST["inputApelido"]);
-    // var_dump($_POST["inputEmail"]);
-    // var_dump($_POST["inputSenha"]);
-    // var_dump($_POST["aceitePoliticasFormCadastro"]);
-    // echo "<br>";
-    // var_dump($nome);
-    // var_dump($sobrenome);
-    // var_dump($apelido);
-    // var_dump($email);
-    // var_dump($senha);
-    // var_dump($aceite);
-    // var_dump($nivel);
-    // exit;
+
     $sql = "INSERT INTO usuarios (id, nome, sobrenome, apelido, email, senha, aceite, nivel) values (:id, :nome, :sobrenome, :apelido, :email, :senha, :aceite, :nivel)";
-    // var_dump($sql);
-    // exit;
+
     $query = $db->prepare($sql);
-    // var_dump($query);
-    // exit;
+
     $salvou = $query->execute([
         ":id" => NULL,
         ":nome" => $nome,
@@ -55,13 +35,10 @@
         ":aceite" => $aceite,
         ":nivel" => $nivel,
     ]);
-    // var_dump($salvou);
-    // exit;
+
     if(isset($salvou) && $salvou == true){
-        // echo "Usuário cadastrado com sucesso";
         header("Location: ../index.php");
     } else {
-        // echo "Falha ao processar cadastro de usuário";
         header("Location: ../cadastrar-usuario.php");
     }
   
