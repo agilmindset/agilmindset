7ec84626d337e4f98eef02d022a1bfa671d57dec
Marcelo-Diament <marcelodiament@gmail.com>
2020-01-31 16:04:28 -0300

    update: gitignore file [MB-000]

--- inc/head.php
+++ inc/head.php
@@ -44,15 +44,16 @@ if(isset($_SESSION) && $_SESSION) {
 
 <head>
     <?php if( $_SERVER["SERVER_NAME"] === "localhost" ||  $_SERVER["SERVER_NAME"] === "127.0.0.1" || strpos($_SERVER["REQUEST_URI"],"/magicbox/") ) {
-        $ambiente = "##### DEBUG MODE | LOCALHOST #####";
+        $ambiente = "debug";
     } else { ?>
         <!-- Google Tag Manager -->
         <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-TQBBNJC');</script>
+    })(window,document,'script','dataLayer','GTM-TQBBNJC');</script>
         <!-- End Google Tag Manager -->
+        <?php $ambiente = "produção"; ?>
     <?php } ?>
     <!-- Configurações Gerais -->
     <meta charset="UTF-8">
